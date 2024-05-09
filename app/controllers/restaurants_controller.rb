a066d9f9710d9a6dec36728007aaf756ecbf82fa
Federico Capaldo <federico.capaldo@hotmail.com>
2024-05-09 20:39:02 +0200

    added scope for all restaurants for index action

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,7 +3,7 @@ class RestaurantsController < ApplicationController
 
   # GET /restaurants
   def index
-    @restaurants = Restaurant.all
+    @restaurants = policy_scope(Restaurant)
   end
 
   # GET /restaurants/1
